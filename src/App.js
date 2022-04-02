94c74e7662bd671df55a3d100d690228b79494c1
dk-jaiswal-77 <dkjaiswal771999@gmail.com>
2022-04-02 19:02:01 +0545

    commit one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {Checkout} from "./components/checkout/Checkout";
+
+import {Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={"home page"} />
+        <Route path="/checkout" element={< Checkout />} />
+      </Routes>
     </div>
   );
 }
